070c34aab9c79742115726bcd2a6beb551d0d289
VinaLx <vinaleux@gmail.com>
2017-12-29 20:52:35 +0800

    router interfaces

---
+++ src/routing/config.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class Address:
+
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+
+class Algorithm(Enum):
+    DV = 1
+    LS = 2
+    LS_CENTRALIZE = 3
+
+
+class Config:
+
+    def __init__(
+            self,
+            algorithm, hostname, self_addr, hns_addr,
+            dead_timeout=180, update_interval=30):
+
+        self.algorithm = algorithm
+        self.hostname = hostname
+        self.self_addr = self_addr
+        self.hns_addr = hns_addr
+        self.dead_timeout = dead_timeout
+        self.update_interval = update_interval

32fd63b07f7c5777cb5d589db1ddbe3ac6d971da
Zexin Yan <yzx9610@outlook.com>
2017-12-29 12:06:46 +0800

    impl add neighbor

--- src/routing/contentFrame.py
+++ src/routing/contentFrame.py
@@ -7,11 +7,13 @@ import os
 class ContentFrame(wx.Frame):
     def __init__(self, parent=None, id=-1, UpdateUI=None):
         wx.Frame.__init__(self, parent, id, title="Router", size=(350, 1000), pos=(500, 200))
-
-        self.hostname_text = None
+        self.hostnames = None
+        self._update_hostnames()
+        self.hostname_choice = None
         self.data_text = None
         self.message_text = None
         self.log_text = None
+
         self.UpdateUI = UpdateUI
         self.init_UI()
         IO.init(self)
@@ -21,35 +23,37 @@ class ContentFrame(wx.Frame):
         self.init_menu()
 
     def init_main(self):
+        """
+        Init Main Frame
+        :return:
+        """
         panel = wx.Panel(self)
-
         hbox = wx.BoxSizer(wx.HORIZONTAL)
-
         fgs = wx.FlexGridSizer(5, 2, 10, 10)
 
         hostname_label = wx.StaticText(panel, label="Hostname:")
-        data_label = wx.StaticText(panel, label="Data:")
+        self.hostname_choice = wx.ComboBox(panel, choices=self.hostnames, style=wx.CB_READONLY)
+        self.Bind(wx.EVT_COMBOBOX_DROPDOWN, self.update_hostnames_handler, self.hostname_choice)
 
-        self.hostname_text = wx.TextCtrl(panel)
+        data_label = wx.StaticText(panel, label="Data:")
         self.data_text = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
 
         send_btn = wx.Button(panel, label="Ok")
-
-        self.Bind(wx.EVT_BUTTON, self._send_data, send_btn)
+        self.Bind(wx.EVT_BUTTON, self._send_data_handler, send_btn)
 
         clear_btn = wx.Button(panel, label="Close")
-        self.Bind(wx.EVT_BUTTON, self._clear, clear_btn)
+        self.Bind(wx.EVT_BUTTON, self._clear_handler, clear_btn)
 
-        messgae = wx.StaticText(panel, label="Message")
+        message = wx.StaticText(panel, label="Message")
         self.message_text = wx.TextCtrl(panel, value="", style=wx.TE_READONLY | wx.TE_MULTILINE)
 
         log = wx.StaticText(panel, label="Log")
         self.log_text = wx.TextCtrl(panel, value="", style=wx.TE_READONLY | wx.TE_MULTILINE)
 
         fgs.AddMany(
-            [hostname_label, (self.hostname_text, 1, wx.EXPAND), (data_label, 1, wx.EXPAND),
+            [hostname_label, (self.hostname_choice, 1, wx.EXPAND), (data_label, 1, wx.EXPAND),
              (self.data_text, 1, wx.EXPAND),
-             (clear_btn, 1, wx.EXPAND), (send_btn, 1, wx.EXPAND), (messgae, 1, wx.EXPAND),
+             (clear_btn, 1, wx.EXPAND), (send_btn, 1, wx.EXPAND), (message, 1, wx.EXPAND),
              (self.message_text, 1, wx.EXPAND),
              (log, 1, wx.EXPAND), (self.log_text, 1, wx.EXPAND)])
 
@@ -65,38 +69,50 @@ class ContentFrame(wx.Frame):
         menu_bar = wx.MenuBar()
 
         file_menu = wx.Menu()
+
         save_message = wx.MenuItem(file_menu, wx.ID_INFO, text="Save Meesage", kind=wx.ITEM_NORMAL)
         self.Bind(wx.EVT_MENU, self._save_message_handler, save_message)
         file_menu.Append(save_message)
+
         save_log = wx.MenuItem(file_menu, wx.ID_FILE, text="Save Log", kind=wx.ITEM_NORMAL)
         self.Bind(wx.EVT_MENU, self._save_log_handler, save_log)
         file_menu.Append(save_log)
+
         close = wx.MenuItem(file_menu, wx.ID_CLOSE, text='&Quit\tCtrl+Q')
         self.Bind(wx.EVT_MENU, self._close_handler, close)
         file_menu.Append(close)
 
+        menu_bar.Append(file_menu, "&File")
+
         config_menu = wx.Menu()
+
         edit_submenu = wx.Menu()
+
         add_neighbor_item = wx.MenuItem(edit_submenu, wx.ID_ADD, text="Add New Neighbor", kind=wx.ITEM_NORMAL)
         self.Bind(wx.EVT_MENU, self._add_neighbor_item_handler, add_neighbor_item)
         edit_submenu.Append(add_neighbor_item)
+
         remove_neighbor_item = wx.MenuItem(edit_submenu, wx.ID_REMOVE, text="Remove Neighbor", kind=wx.ITEM_NORMAL)
         self.Bind(wx.EVT_MENU, self._remove_neighbor_item_handler, remove_neighbor_item)
         edit_submenu.Append(remove_neighbor_item)
+
         config_menu.Append(wx.ID_ANY, "Edit", edit_submenu)
 
-        display_menu = wx.Menu()
-        show_route_table = wx.MenuItem(display_menu, wx.ID_INFO, text="Show route table", kind=wx.ITEM_NORMAL)
-        display_menu.Append(show_route_table)
-        show_topo_graph = wx.MenuItem(display_menu, wx.ID_INFO, text="Show topological structure", kind=wx.ITEM_NORMAL)
-        display_menu.Append(show_topo_graph)
-
-        menu_bar.Append(file_menu, "&File")
         menu_bar.Append(config_menu, "&Config")
-        menu_bar.Append(display_menu, "&Show")
+
         self.SetMenuBar(menu_bar)
         self.SetSize((350, 250))
         self.Center()
+
+    def update_hostnames_handler(self, _):
+        self._update_hostnames()
+        self.hostname_choice.SetItems(self.hostnames)
+
+    def add_message(self, message):
+        self.message_text.AppendText(message)
+
+    def add_log(self, log):
+        self.log_text.AppendText(log)
 
     def _save_message_handler(self, _):
         dlg = wx.FileDialog(self, "Create a file", os.getcwd(), "message.txt", style=wx.FD_SAVE)
@@ -122,27 +138,82 @@ class ContentFrame(wx.Frame):
                 wx.MessageBox(err, "Error", wx.OK | wx.ICON_ERROR)
         dlg.Destroy()
 
-    def _close_handler(self, event):
+    def _close_handler(self, _):
         sys.exit(0)
 
-    def _add_neighbor_item_handler(self, event):
-        IO.print_message("_add_neighbor_handler")
+    def _add_neighbor_item_handler(self, _):
+        myDialog = MyDialog(self, "Add Neighbor")
+        myDialog.ShowModal()
 
-    def _remove_neighbor_item_handler(self, event):
+    def _remove_neighbor_item_handler(self, _):
         self.add_message("remove_neighbor_item")
+        dlg = wx.SingleChoiceDialog(self, "Which hostname you want to remove", "Remove neighbor", self.hostnames)
 
-    def _send_data(self, event):
+        if dlg.ShowModal() == wx.ID_OK:
+            print(dlg.GetStringSelection())
+        dlg.Destroy()
+
+    def _send_data_handler(self, _):
         self.add_message("Send Message\n")
-        print(self.hostname_text.GetValue())
+        print(self.hostname_choice.GetStringSelection())
         print(self.data_text.GetValue())
         wx.MessageBox("This is a Message Box", "Message", wx.OK | wx.ICON_INFORMATION)
 
-    def _clear(self, _):
+    def _clear_handler(self, _):
         self.data_text.Clear()
-        self.hostname_text.Clear()
 
-    def add_message(self, message):
-        self.message_text.AppendText(message)
+    def _update_hostnames(self):
+        # ...
+        self.hostnames = ["A", "B"]
 
-    def add_log(self, log):
-        self.log_text.AppendText(log)
+
+class MyDialog(wx.Dialog):
+    def __init__(self, parent, text):
+        wx.Dialog.__init__(self, parent, -1, text, size=(400, 250))
+        self.hostname_text = None
+        self.cost_text = None
+        self.init_UI()
+
+    def init_UI(self):
+        panel = wx.Panel(self)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        info = wx.StaticBox(panel, -1, 'Information:')
+        nmSizer = wx.StaticBoxSizer(info, wx.VERTICAL)
+
+        nmbox = wx.BoxSizer(wx.HORIZONTAL)
+        hostname = wx.StaticText(panel, -1, "Hostname:")
+        nmbox.Add(hostname, 0, wx.ALL | wx.CENTER, 5)
+
+        self.hostname_text = wx.TextCtrl(panel, -1, style=wx.ALIGN_LEFT)
+        self.cost_text = wx.TextCtrl(panel, -1, style=wx.ALIGN_LEFT)
+        cost = wx.StaticText(panel, -1, "Cost:")
+
+        nmbox.Add(self.hostname_text, 0, wx.ALL | wx.CENTER, 5)
+        nmbox.Add(cost, 0, wx.ALL | wx.CENTER, 5)
+        nmbox.Add(self.cost_text, 0, wx.ALL | wx.CENTER, 5)
+        nmSizer.Add(nmbox, 0, wx.ALL | wx.CENTER, 10)
+
+        sbox = wx.StaticBox(panel, -1, 'Control:')
+        sboxSizer = wx.StaticBoxSizer(sbox, wx.VERTICAL)
+
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        okButton = wx.Button(panel, -1, 'ok')
+        self.Bind(wx.EVT_BUTTON, self._ok_handler, okButton)
+
+        hbox.Add(okButton, 0, wx.ALL | wx.LEFT, 10)
+        cancelButton = wx.Button(panel, -1, 'cancel')
+        self.Bind(wx.EVT_BUTTON, self._close_handler, cancelButton)
+
+        hbox.Add(cancelButton, 0, wx.ALL | wx.LEFT, 10)
+        sboxSizer.Add(hbox, 0, wx.ALL | wx.LEFT, 10)
+        vbox.Add(nmSizer, 0, wx.ALL | wx.CENTER, 5)
+        vbox.Add(sboxSizer, 0, wx.ALL | wx.CENTER, 5)
+        panel.SetSizer(vbox)
+        self.Centre()
+        panel.Fit()
+
+    def _ok_handler(self, _):
+        self.EndModal(wx.ID_CANCEL)
+
+    def _close_handler(self, _):
+        self.EndModal(wx.ID_CANCEL)

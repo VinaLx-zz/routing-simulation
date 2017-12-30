949f2f793e3e42d57e5a55e996eca5f5ac156ed3
Zexin Yan <yzx9610@outlook.com>
2017-12-30 20:28:52 +0800

    fix bugs

--- src/routing/parse.py
+++ src/routing/parse.py
@@ -1,7 +1,8 @@
 import json
 
+
 def parse(data):
-  """ Parse raw data to a json-type dict
-    Args: raw data
-  """
-  return json.loads(data.decode())
+    """ Parse raw data to a json-type dict
+      Args: raw data
+    """
+    return json.loads(data.decode())
